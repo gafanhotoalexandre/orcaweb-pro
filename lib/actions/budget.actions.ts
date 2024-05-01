83297fd88b70de3f0c5d0142a0846f448d78b4f7
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-04-30 22:43:07 -0300

    modificando nome de arquivo

---
+++ lib/actions/budget.actions.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import { User } from '@clerk/nextjs/server'
+import { eq, getTableColumns, sql } from 'drizzle-orm'
+
+import { db } from '@/utils/dbConfig'
+import { Budget, Expense } from '@/utils/schema'
+
+export async function checkUserBudgets(user: User) {
+  const budgets = await db
+    .select()
+    .from(Budget)
+    .where(eq(Budget.createdBy, user.primaryEmailAddress?.emailAddress!))
+
+  return budgets
+}
+
+export async function createBudget({
+  name,
+  amount,
+  icon,
+  email,
+}: CreateBudgetParams) {
+  const result = await db
+    .insert(Budget)
+    .values({
+      name,
+      amount: amount,
+      createdBy: email,
+      icon,
+    })
+    .returning({ insertedId: Budget.id })
+
+  return result
+}
+
+export async function getUserBudgets(user: User) {
+  const budgets = await db
+    .select({
+      ...getTableColumns(Budget),
+      totalSpend: sql<number>`sum(${Expense.amount})`.mapWith(Number),
+      totalItem: sql<number>`count(${Expense.id})`.mapWith(Number),
+    })
+    .from(Budget)
+    .leftJoin(Expense, eq(Budget.id, Expense.budgetId))
+    .where(eq(Budget.createdBy, user.primaryEmailAddress?.emailAddress!))
+    .groupBy(Budget.id)
+
+  return budgets
+}
