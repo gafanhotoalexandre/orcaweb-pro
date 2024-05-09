a05a9ca4e3ac54051312c8bb17eb14359931ba47
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-05-08 22:28:39 -0300

    loading state

--- app/(routes)/dashboard/loading.tsx
+++ app/(routes)/dashboard/loading.tsx
@@ -4,6 +4,13 @@ export default function DashboardLoading() {
   return (
     <div className="p-10">
       <Skeleton className="h-12 w-full md:w-1/3 bg-indigo-50" />
+      <Skeleton className="mt-2 h-6 w-full md:w-3/4 bg-indigo-50" />
+
+      <section className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Skeleton key={index} className="h-28 w-full bg-indigo-50" />
+        ))}
+      </section>
     </div>
   )
 }
